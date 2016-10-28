7f49eb1d55c224c248f22f0bea1f49cc1584f893
Victor Urvantsev <dimberry@gmail.com>
2016-10-28 03:38:59 +0300

    Fixes

--- js/app.js
+++ js/app.js
@@ -3,16 +3,22 @@ function ViewModel() {
     var self = this;
     var map;
     var currentLoc = null;
+    var currentInfoWindow;
     self.markers = [];
-    self.nearMarkers = [];
+    self.tempMarkers = [];
+    self.nearMarkers = ko.observableArray();
     self.photos = ko.observable();
-    self.foursq = ko.observable();
-    self.locationInfo = ko.observableArray();
-    self.visible = ko.observable(false);
-    self.filtersArr = ko.observableArray();
     self.showDesc = ko.observable(false);
-    self.descArr = ko.observableArray();
-    self.descClass = ko.observable('');
+    self.placeIndex = ko.observable();
+
+
+
+    //filters
+    self.filters = ko.observableArray(['All', 'Food', 'Drinks', 'Coffee', 'Shops', 'Arts']);
+    self.currentFilter = ko.observable('All');
+    self.currentFilter.subscribe(function(newValue) {
+        self.updateFilters(newValue);
+    });
 
 
 
@@ -43,51 +49,16 @@ function ViewModel() {
         location: null,
         description: 'One of the great natural wonders of the world, the Iguaçu Falls (Portuguese: Cataratas do Iguaçu, Spanish: Cataratas del Iguazú, Tupi: Y Ûasu "big water") are situated near the border of Brazil, Paraguay, and Argentina. The area is on the UNESCO World Heritage List.'
     }, {
-        placeId: 'ChIJ9Wtt1l9uIysRMCRnhVTydRE',
-        name: 'Uluru (Australia)',
+        placeId: 'ChIJD7fiBh9u5kcRYJSMaMOCCwQ',
+        name: 'Paris (France)',
         location: null,
-        description: 'World Heritage-listed Uluru is one of Australia’s most iconic symbols. Located in the heart of Uluru-Kata Tjuta National Park in Australia’s Red Centre, Uluru is an ancient landscape, rich in Australian indigenous culture and spirituality. There are many ways to experience the majesty of Uluru and the beauty of this unique desert landscape.'
+        description: 'Year after year, the magnetic City of Light draws travelers looking to cross the Eiffel Tower and Notre Dame off their bucket lists. But what visitors really fall in love with are the city\'s quaint cafes, vibrant markets, trendy shopping districts and unmistakable je ne sais quoi charm.'
     }, {
         placeId: 'ChIJLSea6ooWEDERjUGwVxGoqz4',
         name: 'Angkor Wat (Cambodia)',
         location: null,
         description: 'Angkor is one of the most important archaeological sites in South-East Asia. Stretching over some 400 km2, including forested area, Angkor Archaeological Park contains the magnificent remains of the different capitals of the Khmer Empire, from the 9th to the 15th century. They include the famous Temple of Angkor Wat and, at Angkor Thom, the Bayon Temple with its countless sculptural decorations.'
     }];
-
-
-    //set description array to true
-    for (var i = 0; i < locations.length; i++) {
-        self.descArr.push(true);
-    }
-
-
-    //set filters
-    self.filters = [{
-        name: 'All',
-        filter: null
-    }, {
-        name: 'Food',
-        filter: 'food'
-    }, {
-        name: 'Drinks',
-        filter: 'drinks'
-    }, {
-        name: 'Coffee',
-        filter: 'coffee'
-    }, {
-        name: 'Shops',
-        filter: 'shops'
-    }, {
-        name: 'Arts',
-        filter: 'arts'
-    }];
-
-    //set filters to 'All'
-    filtersArr.push(true);
-    for (var i = 1; i < filters.length; i++) {
-        filtersArr.push(false);
-    }
-
 
 
 
@@ -149,111 +120,170 @@ function ViewModel() {
             }]
         }];
 
+
+
+
         map = new google.maps.Map(document.getElementById('map'), {
             styles: styles,
             mapTypeControl: false,
-            maxZoom: 15,
             backgroundColor: 'none'
         });
 
+
         var bounds = new google.maps.LatLngBounds();
+
+
+
 
         //init default locations to map
         self.initLocations = function(locations) {
-            var locId = 0;
             var placeInfoService = new google.maps.places.PlacesService(map);
             var callback = function(place, status) {
                 if (status == google.maps.places.PlacesServiceStatus.OK) {
-                    self.locations[locId].location = place;
-                    createMarker(place);
-                    locId++;
+
+                    //put place object to proper index of locations array
+                    for (var i = 0; i < self.locations.length; i++) {
+                        if (place.place_id === self.locations[i].placeId) {
+                            self.locations[i].location = place;
+                        }
+                    }
+
+                    //and create marker
+                    self.createMarker(place);
+
                     if (locations.length == markers.length) {
-                        showMarkers(markers);
+                        self.showMarkers(markers);
                     }
                 } else {
-                    // throw error
+                    // throw an error
                     alert('initLocations() error: ' + status);
                 }
             };
             for (var i = 0; i < locations.length; i++) {
                 placeInfoService.getDetails(locations[i], callback);
             }
-
         };
 
 
 
-        self.createMarker = function(location, boolean) {
+
+
+        self.createMarker = function(location) {
             // The following group uses the location array to create an array of markers on initialize.
             // Create a marker per location, and put into markers array.
-            var animation;
-            var icon;
-            if (boolean) {
-                animation = google.maps.Animation.BOUNCE;
-                icon = 'img/position.png';
 
-            } else {
-                animation = google.maps.Animation.DROP;
-            }
             var marker = new google.maps.Marker({
                 map: map,
                 position: location.geometry.location,
                 placeId: location.place_id,
                 title: location.name,
                 location: location,
-                animation: animation,
-                icon: icon
+                animation: google.maps.Animation.DROP,
             });
 
             // Push the marker to our array of markers.
             markers.push(marker);
             // Create an onclick event to show the route from user's location
             marker.addListener('click', function() {
-                // drawRoute(this);
                 self.getInfo(location);
             });
         };
 
 
-        self.createNearMarker = function(location) {
 
+
+        self.createNearMarker = function(location, filter, html, rating) {
             // The following group uses the location array to create an array of markers on initialize.
             // Create a marker per location, and put into markers array.
+
+            var infowindow = new google.maps.InfoWindow({
+                maxWidth: 200,
+                content: rating
+            });
+
             var marker = new google.maps.Marker({
                 map: map,
                 position: location,
                 animation: google.maps.Animation.DROP,
-                icon: 'img/blue-dot.png'
+                filter: filter,
+                icon: 'img/blue-dot.png',
+                html: html,
+                rating: rating,
+                infowindow: infowindow,
             });
+
+            infowindow.addListener('closeclick', function() {
+                marker.setAnimation(null);
+                marker.infowindow = null;
+                marker.setIcon('img/blue-dot.png');
+            });
+
             // Push the marker to our array of markers.
             self.nearMarkers.push(marker);
             // Create an onclick event to show the route from user's location
-            marker.addListener('click', function() {});
+            marker.addListener('click', function() {
+                self.markerClick(marker);
+            });
         };
 
 
+
+        self.markerClick = function(marker) {
+            self.disablePreviousMarker();
+            currentInfoWindow = marker.infowindow;
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+            marker.setIcon('img/yellow-dot.png');
+            marker.infowindow.open(map, marker);
+        };
+
+
+
+
+        self.disablePreviousMarker = function() {
+
+            self.nearMarkers().forEach(function(marker) {
+                marker.setIcon('img/blue-dot.png');
+                marker.setAnimation(null);
+            });
+            if (currentInfoWindow) {
+                currentInfoWindow.close();
+            }
+        };
+
+
+
         self.showMarkers = function(markers) {
-            //clear all previous markers
+            //check if any markers were found
+            //empty markers array could happen when
+            // no palce from foursquare was found
+            var bounds = new google.maps.LatLngBounds();
             if (markers.length !== 0) {
-                var bounds = new google.maps.LatLngBounds();
                 for (var i = 0; i < markers.length; i++) {
                     markers[i].setMap(map);
                     bounds.extend(markers[i].position);
+                }
+
+                //fit current Location to the bounds
+                if (currentLoc) {
+                    bounds.extend(currentLoc.geometry.location);
                 }
                 map.fitBounds(bounds);
             }
         };
 
 
+
+
         self.clearAllMarkers = function() {
             for (var i = 0; i < self.markers.length; i++) {
                 self.markers[i].setMap(null);
             }
-            for (var k = 0; k < nearMarkers.length; k++) {
-                self.nearMarkers[k].setMap(null);
+            for (var k = 0; k < self.nearMarkers().length; k++) {
+                self.nearMarkers()[k].setMap(null);
             }
-            self.nearMarkers = [];
+            self.nearMarkers.removeAll();
         };
+
 
 
 
@@ -262,12 +292,14 @@ function ViewModel() {
             if (showDesc() === false) {
                 clearAllMarkers();
                 self.showDesc(true);
-                self.createMarker(location, true);
                 self.getPhotos(location);
                 self.getFoursq(location);
-                self.updateDesc(index);
+                self.placeIndex(index);
             }
         };
+
+
+
 
         self.getPhotos = function(location) {
             //get the first photo of the location with Google Maps
@@ -280,98 +312,89 @@ function ViewModel() {
         };
 
 
-        self.getFoursq = function(location, filter) {
+
+
+        self.getFoursq = function(location) {
             //get places around
             currentLoc = location;
-            self.visible(true);
-            var html = '';
             var placesHtml = document.getElementById('foursq');
             var client_id = '5CWHKEMR3L3IE243DGBN0F1I4AO2JB3RBRT0BF1SPO3AKL2K';
             var client_secret = 'WFYUAIRF0HPREFZLPBDVQAPC2KE5QPOAROWBWHALXNMTL0XZ';
-            var limit = '20'; //show first 20 venues
-            var radius = '2500'; //radius in meters from location
+            var limit = 5; //show first 5 venues for each category
+            var radius = 5000; //radius in meters from location
 
             var url = 'https://api.foursquare.com/v2/venues/explore?client_id=' + client_id +
                 '&client_secret=' + client_secret + '&ll=' + location.geometry.location.lat() +
                 ',' + location.geometry.location.lng() + '&radius=' + radius + '&v=20161016&limit=' +
                 limit + '&sortByDistance=1';
 
-            if (filter) {
-                url += '&section=' + filter;
-            } else {
-                url += '&section=food,drinks,coffee,shops,arts';
+
+            var runs = 0;
+
+            for (var i = 1; i < self.filters().length; i++) {
+                var newUrl = url + '&section=' + self.filters()[i];
+                asyncload(newUrl, self.filters()[i]);
             }
 
-            $.getJSON(url, function(data) {
-
-                    $.each(data.response.groups[0].items, function(key, val) {
-                        //push all venue's categories to filter array
-                        var icon = val.venue.categories[0].icon.prefix + '32.png';
-                        var latlng = new google.maps.LatLng(val.venue.location.lat, val.venue.location.lng);
-                        createNearMarker(latlng);
-                        html += '<li class="place"><a class="place-link" href="https://foursquare.com/v/' + val.venue.id + '?ref=' + client_id + '"target="_blank">' + val.venue.name + '</a><img class="icon" src="' + icon + '"></li> ';
+            function asyncload(url, currFilter) {
+                $.getJSON(url, function(data) {
+                        $.each(data.response.groups[0].items, function(key, val) {
+                            //push all venue's categories to filter array
+                            var icon = val.venue.categories[0].icon.prefix + '32.png';
+                            var latlng = new google.maps.LatLng(val.venue.location.lat, val.venue.location.lng);
+                            var html = '<li class="place"><a class="place-link" href="#">' + val.venue.name + '</a><img class="icon" src="' + icon + '"></li> ';
+                            var infoWin;
+                            if (!val.venue.rating) {
+                                infoWin = '<div class="rating">' + val.venue.name + '<br>Rating: Not Rated Yet<br><a href="http://foursquare.com/v/' + val.venue.id + '?ref=' + client_id + '" target="_blank"><img src="img/rsz_foursquare_logo.png"></a></div>';
+                            } else {
+                                infoWin = val.venue.name + '<br><div style="color:#' + val.venue.ratingColor + '" class="rating">Rating: ' + val.venue.rating + '<br><a href="https://foursquare.com/v/' + val.venue.id + '?ref=' + client_id + '" target="_blank"><img src="img/rsz_foursquare_logo.png"></a></div>';
+                            }
+                            self.createNearMarker(latlng, currFilter, html, infoWin);
+                        });
+                    })
+                    .done(function() {
+                        runs++;
+                        if (runs === self.filters().length - 1) {
+                            self.showMarkers(self.nearMarkers());
+                        }
+                    })
+                    .fail(function(data) {
+                        alert("error:" + data);
                     });
-                })
-                .done(function() {
-                    if (html === '') {
-                        html = 'Nothing found :(';
-                    }
-                    locationInfo(html);
-                    showMarkers(self.nearMarkers);
-                })
-                .fail(function(data) {
-                    alert("error:" + data);
-                });
+            }
         };
 
 
-        self.updateFilters = function(filter, index) {
-            clearAllMarkers();
-            self.createMarker(currentLoc, true);
-            self.getFoursq(currentLoc, filter);
-            for (var i = 0; i < self.filtersArr().length; i++) {
-                if (i == index) {
-                    self.filtersArr()[i] = true;
+
+
+        //show only relevant markers on the map
+        self.updateFilters = function(filter) {
+            if (currentInfoWindow) {
+                currentInfoWindow.close();
+            }
+            for (var k = 0; k < self.nearMarkers().length; k++) {
+                if (filter === 'All' || self.nearMarkers()[k].filter === filter) {
+                    self.nearMarkers()[k].setMap(map);
                 } else {
-                    self.filtersArr()[i] = false;
+                    self.nearMarkers()[k].setMap(null);
                 }
+                self.nearMarkers()[k].setIcon('img/blue-dot.png');
             }
-            self.filtersArr.valueHasMutated();
         };
 
-        self.updateDesc = function(index) {
-            for (var i = 0; i < self.descArr().length; i++) {
-                if (i == index) {
-                    self.descArr()[i] = true;
-                } else {
-                    self.descArr()[i] = false;
-                }
-            }
-            self.descArr.valueHasMutated();
-            self.descClass('desc nohover');
-        };
 
-        self.resetDesc = function() {
-            for (var i = 0; i < self.descArr().length; i++) {
-                self.descArr()[i] = true;
-            }
-            self.descArr.valueHasMutated();
-        };
 
 
         //for reset arrow
         self.reset = function() {
             self.clearAllMarkers();
-            self.markers = [];
+            self.showMarkers(markers);
             self.showDesc(false);
-            self.descClass('');
-            self.visible(false);
             self.photos('');
-            self.foursq('');
-            self.locationInfo('');
-            self.resetDesc();
-            initLocations(locations);
+            self.currentFilter('All');
         };
+
+
 
 
         self.initLocations(locations);
